10666ab8345d7b17216aaad197690d4991ef9c0b
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2003-03-19 21:52:25 +0000

    Initial revision

---
+++ trunk/simme/src/test/sim/Game.java
@@ -0,0 +1,16 @@
+package test.sim;
+
+/**
+ * Represents a game with all its states. Provides methods to start, end,
+ * pause a game and uses a <code>Canvas</code> to draw itself to the display
+ *
+ * @author kariem
+ */
+public class Game {
+
+   public Game() {
+      super();
+      // TODO Auto-generated constructor stub
+   }
+
+}
