add2cf19c59a6df9a7e137ad37aa96d83d865811
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2003-04-20 08:54:37 +0000

    updated

--- trunk/simme/src/test/sim/StatusAlert.java
+++ trunk/simme/src/test/sim/StatusAlert.java
@@ -7,26 +7,31 @@
 package test.sim;
 
 import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Display;
 
+import test.sim.net.StatusMessage;
 import test.sim.net.XmlMessage;
 
 /**
  * @author jorge
  *
- * To change the template for this generated type comment go to
- * Window>Preferences>Java>Code Generation>Code and Comments
  */
-public class StatusAlert extends XmlMessage {
+public class StatusAlert extends Alert {
 
    private XmlMessage StatusMsg;
 
-   public StatusAlert(StatusMsg statusMsg, Display d) {
-
-      Alert statusAlert = new Alert("Status");
-      statusAlert.setTimeout(Alert.FOREVER);
-      statusAlert.setString(statusMsg);
-      d.setCurrent(statusAlert);
+	/**
+	 * Creates a new status alert showing the message contained in the
+	 * <code>statusMsg</code>.
+	 * 
+	 * @param statusMsg the status message containing information.
+	 * @param d <code>Display</code> to use for showing this alert.
+	 */
+   public StatusAlert(StatusMessage statusMsg, Display d) {
+      super("Info", statusMsg.getString(), null, AlertType.INFO);
+      setTimeout(3000);
+      d.setCurrent(this);
    }
 
 }
