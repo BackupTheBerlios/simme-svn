3db0c7cc1ae9a3759c2ff9c926e6c535fa34f7f7
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2003-04-20 09:03:02 +0000

    initial

---
+++ trunk/simme/src/test/sim/net/StatusMessage.java
@@ -0,0 +1,39 @@
+package test.sim.net;
+
+import java.io.IOException;
+
+/**
+ * Statusmessage.
+ *  
+ * @author kariem
+ */
+public class Statusmessage extends XmlMessage {
+
+   /**
+    * @param data
+    * @throws IOException
+    */
+   public StatusMessage(byte[] data) throws IOException {
+      super(data);
+   }
+
+   /**
+    * @param data
+    * @throws IOException
+    */
+   public StatusMessage(String data) throws IOException {
+      super(data);
+   }
+
+   /**
+    * Returns the content of the statusmessage, or <code>null</code>
+    * 
+    *
+    * @return
+    */
+   public String getString() {
+      String message = (String) xmlElement.getAttribute("msg");
+      return message;
+   }
+
+}
