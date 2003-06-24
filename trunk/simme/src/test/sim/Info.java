4d79ecf671b0e170a93509205b48641b7762addb
kstefan <kstefan@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2003-06-24 13:47:45 +0000

    Info mit Undo erweitert und NullpointerException bei paint(g) entfernt.

--- trunk/simme/src/test/sim/Info.java
+++ trunk/simme/src/test/sim/Info.java
@@ -11,43 +11,45 @@ import javax.microedition.lcdui.Display;
  */
 public class Info {
    
-   private static final StringBuffer BUF = new StringBuffer();
+  private static final StringBuffer BUF = new StringBuffer();
 
-   static {
-      BUF.append("Das Spiel besteht aus 6 Knoten von denen jeder mit jedem ");
-      BUF.append("verbunden ist. Der Spieler der an der Reihe ist wählt ");
-      BUF.append("eine Verbindung zwischen 2 Knoten aus. Diese wird dann in ");
-      BUF.append("seiner Farbe angezeigt. Dann wählt der 2. Spieler eine ");
-      BUF.append("Verbindung und markiert sie in seiner Farbe usw.\n\n");
-      BUF.append("Der erste Spieler der in seiner Farbe ein Dreieck formt ");
-      BUF.append("hat verloren. Ziel ist es also zu vermeiden, ein Dreieck zu");      BUF.append("formen und den Gegenspieler zum \"dreiecken\" zu drängen.");
-      BUF.append("\n\n");
-      BUF.append("Um eine Verbindung auszuwählen, drückt man auf eine ");
-      BUF.append("Zahl zwischen 1 und 6 um den ersten Knoten zu markieren ");
-      BUF.append("und das gleiche nochmals um den zweiten Knoten und somit ");
-      BUF.append("eine Verbindung zu markieren.\n\n");
-      BUF.append("Sollte man sich nach der Wahl des ersten Knotens anders ");
-      BUF.append("entschieden haben, kann man durch nochmaliges Klicken auf ");
-      BUF.append("dieselbe Taste den Knoten wieder deaktivieren.\n\n\n");
-      BUF.append("Viel Spaß!");
+  static {
+    BUF.append("Das Spiel besteht aus 6 Knoten von denen jeder mit jedem ");
+    BUF.append("verbunden ist. Der Spieler der an der Reihe ist wählt ");
+    BUF.append("eine Verbindung zwischen 2 Knoten aus. Diese wird dann in ");
+    BUF.append("seiner Farbe angezeigt. Dann wählt der 2. Spieler eine ");
+    BUF.append("Verbindung und markiert sie in seiner Farbe usw.\n\n");
+    BUF.append("Der erste Spieler der in seiner Farbe ein Dreieck formt ");
+    BUF.append("hat verloren. Ziel ist es also zu vermeiden, ein Dreieck zu");
+    BUF.append("formen und den Gegenspieler zum \"dreiecken\" zu drängen.");
+    BUF.append("\n\n");
+    BUF.append("Um eine Verbindung auszuwählen, drückt man auf eine ");
+    BUF.append("Zahl zwischen 1 und 6 um den ersten Knoten zu markieren ");
+    BUF.append("und das gleiche nochmals um den zweiten Knoten und somit ");
+    BUF.append("eine Verbindung zu markieren.\n\n");
+    BUF.append("Sollte man sich nach der Wahl des ersten Knotens anders ");
+    BUF.append("entschieden haben, kann man durch nochmaliges Klicken auf ");
+    BUF.append("dieselbe Taste den Knoten wieder deaktivieren.\n\n");
+    BUF.append("Mit der Taste 0 ist jederzeit ein Undo möglich!\n\n");
+    BUF.append("Viel Spass!");
       
-   }
+  }
 
-   private static final String HOWTO = BUF.toString();
+  private static final String HOWTO = BUF.toString();
       
 
-   private Info() {
-   }
+  private Info() {
+  }
 
-   /**
-    * Shows the information
-    *
-    * @param display The display, where the information is to be shown.
-    */
-   public static void showInfo(Display display) {
-      Alert alert = new Alert("SimME HowTo", HOWTO, null, AlertType.INFO);
-      alert.setTimeout(Alert.FOREVER);
+  /**
+   * Shows the information
+   *
+   * @param display The display, where the information is to be shown.
+   */
+  public static void showInfo(Display display) {
+    Alert alert = new Alert("SimME HowTo", HOWTO, null, AlertType.INFO);
+    alert.setTimeout(Alert.FOREVER);
 
-      display.setCurrent(alert);
-   }
+    display.setCurrent(alert);
+  }
 }
