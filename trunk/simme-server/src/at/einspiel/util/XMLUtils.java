21ad681ea191a3e5366dc450cb2ecafaf9b84a24
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2004-04-03 23:38:36 +0000

    Changes merged into 'umfragetool'

--- trunk/simme-server/src/at/einspiel/util/XMLUtils.java
+++ trunk/simme-server/src/at/einspiel/util/XMLUtils.java
@@ -1,8 +1,8 @@
 // ----------------------------------------------------------------------------
 // [Simme-Server]
 //       Java Source File: XMLUtils.java
-//                  $Date: 2004/02/21 23:04:20 $
-//              $Revision: 1.1 $
+//                  $Date: 2004/04/03 23:38:36 $
+//              $Revision: 1.2 $
 // ----------------------------------------------------------------------------
 package at.einspiel.util;
 
@@ -30,8 +30,7 @@ public class XMLUtils {
     * @return a <code>List</code> of DOM elements, that are direct children of
     *          the given parent node.
     */
-   public static List getDirectChildren(Element parentElement)
-   {
+   public static List getDirectChildren(Element parentElement) {
       List directChildren = new ArrayList();
       Node child = parentElement.getFirstChild();
       while (child != null) {
@@ -41,5 +40,23 @@ public class XMLUtils {
          child = child.getNextSibling();
       }
       return directChildren;
-   }   
+   }
+   
+   /**
+    * Returns the text contained in the given element.
+    * @param e the element.
+    * @return the text in the first textnode within the element, or
+    *          <code>null</code>, if the element does not contain any text
+    *          nodes.
+    */
+   public static String getText(Element e) {
+       Node child = e.getFirstChild();
+       while (child != null) {
+           if (child.getNodeType() == Node.TEXT_NODE) {
+               // return the text of the text node
+               return child.getNodeValue();
+           }
+       }
+       return null;
+   }
 }
