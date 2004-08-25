3faf279f00183e5972b13898c46ab09679b445d3
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2004-08-25 15:46:18 +0000

    updated because of changes in SendableUI

--- trunk/simme-server/src/at/einspiel/messaging/SimpleClientMessage.java
+++ trunk/simme-server/src/at/einspiel/messaging/SimpleClientMessage.java
@@ -1,6 +1,5 @@
 package at.einspiel.messaging;
 
-
 /**
  * This class implements the message interface and allows for easy transfer of
  * information to the client/user.
@@ -9,41 +8,42 @@ package at.einspiel.messaging;
  */
 public class SimpleClientMessage extends SendableUI implements Message {
 
-    /**
-     * Creates a simple message with the given title and content.
-     * 
-     * @param title the title.
-     * @param msg the message.
-     */
-    public SimpleClientMessage(String title, String msg) {
-        super();
-        setTitle(title);
-        setText(msg);
-    }
+	/**
+	 * Creates a simple message with the given title and content.
+	 * 
+	 * @param title
+	 *            the title.
+	 * @param msg
+	 *            the message.
+	 */
+	public SimpleClientMessage(String title, String msg) {
+		super(title, msg);
+	}
 
-    /**
-     * Creates a simple message with the given content.
-     * 
-     * @param msg the message.
-     * 
-     * @see #SimpleClientMessage(String, String)
-     */
-    public SimpleClientMessage(String msg) {
-        this("Information", msg);
-    }
+	/**
+	 * Creates a simple message with the given content.
+	 * 
+	 * @param msg
+	 *            the message.
+	 * 
+	 * @see #SimpleClientMessage(String, String)
+	 */
+	public SimpleClientMessage(String msg) {
+		this("Information", msg);
+	}
 
-    /** @see Message#getMessage() */
-    public String getMessage() {
-        return getXmlString();
-    }
-    
-    /** @see Message#getId() */
-    public byte getId() {
-        return 0;
-    }
-    
-    /** @see Message#getInfo() */
-    public String getInfo() {
-        return getText();
-    }
+	/** @see Message#getMessage() */
+	public String getMessage() {
+		return getXmlString();
+	}
+
+	/** @see Message#getId() */
+	public byte getId() {
+		return 0;
+	}
+
+	/** @see Message#getInfo() */
+	public String getInfo() {
+		return getInfoObject().getText();
+	}
 }
