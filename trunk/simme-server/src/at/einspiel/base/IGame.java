a32df719fafc455c0c5d0d84b1051801124c8ecb
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2004-09-13 15:12:58 +0000

    added getGame

--- trunk/simme-server/src/at/einspiel/base/IGame.java
+++ trunk/simme-server/src/at/einspiel/base/IGame.java
@@ -1,13 +1,14 @@
 // ----------------------------------------------------------------------------
 // [Simme-Server]
 //       Java Source File: IGame.java
-//                  $Date: 2003/12/28 18:21:43 $
-//              $Revision: 1.2 $
+//                  $Date: 2004/09/13 15:12:58 $
+//              $Revision: 1.3 $
 // ----------------------------------------------------------------------------
 package at.einspiel.base;
 
 import java.util.Date;
 
+import at.einspiel.simme.client.Game;
 import at.einspiel.simme.client.Move;
 
 /**
@@ -21,55 +22,55 @@ public interface IGame {
     * @param p1 player 1.
     * @param p2 player 2.
     */
-   public abstract void setPlayers(User p1, User p2);
+   void setPlayers(User p1, User p2);
 
    /** Starts the game */
-   public abstract void startGame();
+   void startGame();
 
    /** Stops the game */
-   public abstract void stopGame();
+   void stopGame();
 
    /** Cancels this game. */
-   public abstract void cancelGame();
+   void cancelGame();
 
    /**
     * Returns the length of this game in ms
     *
     * @return The length of this game.
     */
-   public abstract long getLength();
+    long getLength();
 
    /**
     * Returns the date of the game. The accuracy is in seconds.
     * @return The date, of this game.
     */
-   public abstract Date getDate();
+    Date getDate();
 
    /**
     * Sets the date of this game. All information more exact than seconds will
     * be dropped.
     * @param d The new date.
     */
-   public abstract void setDate(Date d);
+    void setDate(Date d);
 
    /**
     * Returns whether the game is currently running.
     * 
     * @return <code>true</code> if the game is running.
     */
-   public abstract boolean isRunning();
+    boolean isRunning();
 
    /**
     * Returns the game's id.
     * @return the id of the game.
     */
-   public abstract String getId();
+    String getId();
 
    /**
     * Returns the current move number.
     * @return the current move number.
     */
-   public abstract byte getMoveNr();
+    byte getMoveNr();
 
    /**
     * Performs the given move for a user.
@@ -77,7 +78,7 @@ public interface IGame {
     * @param m the move to perform.
     * @return the move's result.
     */
-   public abstract Result makeMove(User u, Move m);
+    Result makeMove(User u, Move m);
 
    /**
     * Performs the given move.
@@ -85,7 +86,7 @@ public interface IGame {
     * @param m the move to perform.
     * @return the move's result.
     */
-   public abstract Result makeMove(Move m);
+    Result makeMove(Move m);
 
    /**
     * Returns whether it is the given user's turn to play.
@@ -95,5 +96,12 @@ public interface IGame {
     *          has already ended, or the given user is not allowed to make the
     *          next move.
     */
-   public abstract boolean isPlayerOnTurn(User user);
+    boolean isPlayerOnTurn(User user);
+   
+    /**
+     * Returns the game object.
+     * @return the game object.
+     */
+    Game getGame();
+   
 }
