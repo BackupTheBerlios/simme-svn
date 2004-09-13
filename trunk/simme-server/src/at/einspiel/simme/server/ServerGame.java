72322e0cf17cb9318957e10da6cbd3d50a6f31f1
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2004-09-13 15:15:35 +0000

    implemented new IGame, added "nice id" to be forwarded to the client

--- trunk/simme-server/src/at/einspiel/simme/server/ServerGame.java
+++ trunk/simme-server/src/at/einspiel/simme/server/ServerGame.java
@@ -1,11 +1,12 @@
 // ----------------------------------------------------------------------------
 // [Simme-Server]
 //       Java Source File: ServerGame.java
-//                  $Date: 2004/06/07 09:25:22 $
-//              $Revision: 1.4 $
+//                  $Date: 2004/09/13 15:15:35 $
+//              $Revision: 1.5 $
 // ----------------------------------------------------------------------------
 package at.einspiel.simme.server;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -14,300 +15,336 @@ import at.einspiel.base.IGame;
 import at.einspiel.base.Result;
 import at.einspiel.base.User;
 import at.einspiel.simme.client.Game;
+import at.einspiel.simme.client.GameInfo;
 import at.einspiel.simme.client.Move;
 
 /**
  * Represents a simme game that is played between two users.
- *
+ * 
  * @author kariem
  */
 public class ServerGame implements IGame {
 
-    private static final int ACCURACY = 1000;
+	private static final SimpleDateFormat FORMAT_ID = new SimpleDateFormat("yyyyMMdd-HHmmss");
+	private static final int ACCURACY = 1000;
 
-    /** date of game in seconds */
-    private long date;
-    /** times each player needed to perform moves */
-    private long time1, time2;
+	/** date of game in seconds */
+	private long date;
+	/** times each player needed to perform moves */
+	private long time1, time2;
 
-    /** moves that were performed */
-    private List savedMoves; // used to reconstruct game later
-    
-    private final StopWatch stopwatch;
-    private Game game;
-    private boolean running;
-    private boolean gameover;
+	/** moves that were performed */
+	private List savedMoves; // used to reconstruct game later
 
-    /** first player */
-    protected User player1;
-    /** second player */
-    protected User player2;
+	private final StopWatch stopwatch;
+	private Game game;
+	private boolean running;
+	private boolean gameover;
 
-    /** game id */
-    private String id;
+	/** first player */
+	protected User player1;
+	/** second player */
+	protected User player2;
 
-    byte nbMoves;
-    User winner;
+	byte nbMoves;
+	User winner;
 
-    /**
-     * Creates a new ServerGame object.
-     *
-     * @param p1 first player
-     * @param p2 second player
-     *
-     * @throws IllegalArgumentException if rules are broken (e.g. same players).
-     */
-    public ServerGame(User p1, User p2) throws IllegalArgumentException {
-        String nick1 = p1.getNick();
-        String nick2 = p2.getNick();
-        if (nick1.equals(nick2)) {
-            throw new IllegalArgumentException("Users have same nick names.");
-        }
+	/**
+	 * Creates a new ServerGame object.
+	 * 
+	 * @param p1
+	 *            first player
+	 * @param p2
+	 *            second player
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if rules are broken (e.g. same players).
+	 */
+	public ServerGame(User p1, User p2) throws IllegalArgumentException {
+		String nick1 = p1.getNick();
+		String nick2 = p2.getNick();
+		if (nick1.equals(nick2)) {
+			throw new IllegalArgumentException("Users have same nick names.");
+		}
 
-        game = new Game() {/* subclassed abstract class */ };
-        if (Math.random() > 0.5) {
-            setPlayers(p1, p2);
-        } else {
-            setPlayers(p2, p1);
-        }
-        setDate(new Date());
+		game = new Game() {/* subclassed abstract class */
+		};
+		if (Math.random() > 0.5) {
+			setPlayers(p1, p2);
+		} else {
+			setPlayers(p2, p1);
+		}
+		setDate(new Date());
 
-        // set id to something unique
-        id = date + "#~#" + nick1 + "#~#" + nick2;
+		// set id to something unique
+		game.setId(date + "---" + player1.getNick() + "---" + player2.getNick());
 
-        // initialize game status
-        running = false;
-        gameover = false;
-        // create stopwatch
-        stopwatch = new StopWatch();
-    }
+		// initialize game status
+		running = false;
+		gameover = false;
+		// create stopwatch
+		stopwatch = new StopWatch();
+	}
 
-    /**
-     * Sets the players for this game.
-     * @param p1 player 1.
-     * @param p2 player 2.
-     */
-    public void setPlayers(User p1, User p2) {
-        this.player1 = p1;
-        this.player2 = p2;
-    }
+	/**
+	 * Sets the players for this game. Additionally the game information is
+	 * updated.
+	 * @param p1
+	 *            player 1.
+	 * @param p2
+	 *            player 2.
+	 */
+	public void setPlayers(User p1, User p2) {
+		this.player1 = p1;
+		this.player2 = p2;
 
-    /** Starts the game */
-    public void startGame() {
-        running = true;
-        gameover = false;
-        savedMoves = new ArrayList();
-        game.start();
-        stopwatch.start();
-    }
+		// update game information, if possible
+		GameInfo info = game.getGameInfo();
+		info.setP1Name(p1.getNick());
+		info.setP1Info(p1.getInfo());
+		info.setP2Name(p2.getNick());
+		info.setP2Info(p2.getInfo());
+	}
 
-    /** Rotates players and restarts the game. */
-    public void restartGame() {
-       setPlayers(player2, player1);
-       startGame();
-    }
-    
-    
-    /** Stops the game */
-    public void stopGame() {
-        if (!gameover) {
-            stopwatch.stop();
-            running = false;
-            gameover = true;
-            nbMoves = game.getMoveNr();
-        }
-    }
+	/**
+	 * Starts the game. Prepares everything for the game start and starts the
+	 * timer.
+	 */
+	public void startGame() {
+		running = true;
+		gameover = false;
+		savedMoves = new ArrayList();
+		game.start();
+		stopwatch.start();
+	}
 
-    /** Cancels this game. */
-    public void cancelGame() {
-        // TODO implement cancel game on server
-    }
+	/** Rotates players and restarts the game. */
+	public void restartGame() {
+		setPlayers(player2, player1);
+		startGame();
+	}
 
-    /**
-     * Returns the length of this game in ms
-     *
-     * @return The length of this game.
-     */
-    public long getLength() {
-        return stopwatch.getTotalDuration();
-    }
+	/** Stops the game */
+	public void stopGame() {
+		if (!gameover) {
+			stopwatch.stop();
+			running = false;
+			gameover = true;
+			nbMoves = game.getMoveNr();
+		}
+	}
 
-    /**
-     * Returns the date of the game. The accuracy is in seconds.
-     * @return The date, of this game.
-     */
-    public Date getDate() {
-        return new Date(date);
-    }
+	/** Cancels this game. */
+	public void cancelGame() {
+		// TODO implement cancel game on server
+	}
 
-    /**
-     * Sets the date of this game. All information more exact than seconds will
-     * be dropped.
-     * @param d The new date.
-     */
-    public void setDate(Date d) {
-        long time = d.getTime();
-        this.date = (time - (time % ACCURACY));
-    }
+	/**
+	 * Returns the length of this game in ms
+	 * 
+	 * @return The length of this game.
+	 */
+	public long getLength() {
+		return stopwatch.getTotalDuration();
+	}
 
-    /**
-     * Returns whether the game is currently running.
-     * 
-     * @return <code>true</code> if the game is running.
-     */
-    public boolean isRunning() {
-        return running;
-    }
+	/**
+	 * Returns the date of the game. The accuracy is in seconds.
+	 * @return The date, of this game.
+	 */
+	public Date getDate() {
+		return new Date(date);
+	}
 
-    /**
-     * Returns the game's id.
-     * @return the id of the game.
-     */
-    public String getId() {
-        return id;
-    }
+	/**
+	 * Sets the date of this game. All information more exact than seconds will
+	 * be dropped.
+	 * @param d
+	 *            The new date.
+	 */
+	public void setDate(Date d) {
+		long time = d.getTime();
+		this.date = (time - (time % ACCURACY));
+	}
 
-    /** Class that is used to stop the length of this game */
-    class StopWatch {
-        private long startTime;
-        private long lastPause;
-        private long lastDuration;
-        private long totalDuration;
-        private boolean started;
+	/**
+	 * Returns whether the game is currently running.
+	 * 
+	 * @return <code>true</code> if the game is running.
+	 */
+	public boolean isRunning() {
+		return running;
+	}
 
-        void start() {
-            startTime = System.currentTimeMillis();
-            lastPause = startTime;
-            totalDuration = 0;
-            started = true;
-        }
+	/**
+	 * Returns the game's id.
+	 * @return the id of the game.
+	 */
+	public String getId() {
+		return game.getId();
+	}
 
-        long elapsed() {
-            pause(System.currentTimeMillis());
-            return lastDuration;
-        }
+	/**
+	 * Returns an id that is easier to read, than the real id.
+	 * @return an easy to read id, that can be presented to the user.
+	 * @see #getId()
+	 */
+	public String getNiceId() {
+		return FORMAT_ID.format(getDate());
+	}
 
-        void stop() {
-            long time = System.currentTimeMillis();
-            pause(time);
-            totalDuration = time - startTime;
-            started = false;
-        }
+	/** @see at.einspiel.base.IGame#getGame() */
+	public Game getGame() {
+		return game;
+	}
 
-        long getTotalDuration() {
-            if (started) {
-                return System.currentTimeMillis() - startTime;
-            }
-            return totalDuration;
-        }
+	/** Class that is used to stop the length of this game */
+	class StopWatch {
+		private long startTime;
+		private long lastPause;
+		private long lastDuration;
+		private long totalDuration;
+		private boolean started;
 
-        private void pause(long time) {
-            lastDuration = time - lastPause;
-            lastPause = time;
-        }
+		void start() {
+			startTime = System.currentTimeMillis();
+			lastPause = startTime;
+			totalDuration = 0;
+			started = true;
+		}
 
-    }
+		long elapsed() {
+			pause(System.currentTimeMillis());
+			return lastDuration;
+		}
 
-    //
-    // game method accessors
-    //
+		void stop() {
+			long time = System.currentTimeMillis();
+			pause(time);
+			totalDuration = time - startTime;
+			started = false;
+		}
 
-    /**
-     * Returns the move number.
-     * @return the move number.
-     */
-    public byte getMoveNr() {
-        return game.getMoveNr();
-    }
+		long getTotalDuration() {
+			if (started) {
+				return System.currentTimeMillis() - startTime;
+			}
+			return totalDuration;
+		}
 
-    /**
-     * Performs the given move for a user.
-     * @param u the user.
-     * @param m the move to perform.
-     * @return the move's result.
-     */
-    public Result makeMove(User u, Move m) {
-       return selectEdge(u, m.getEdge()) ? Result.POSITIVE : Result.NEGATIVE;
-    }    
-    
-    /**
-     * Performs the given move.
-     * 
-     * @param m the move to perform.
-     * @return the move's result.
-     */
-    public Result makeMove(Move m) {
-        return makeMove(null, m);
-    }
+		private void pause(long time) {
+			lastDuration = time - lastPause;
+			lastPause = time;
+		}
 
-    /**
-     * Selects an edge in the game on the server.
-     * 
-     * @param u the user who performs the move.
-     * @param edge the edge to select (between 0 and 14).
-     * @return whether the selection has succeeded.
-     */
-    public boolean selectEdge(User u, byte edge) {
-        if (isRunning()) {
-            byte playersTurn = game.getPlayersTurn();
-            if (isOnTurn(u, playersTurn)) {
+	}
 
-                // make move
-                boolean edgeSelection = game.selectEdge(edge);
-                if (edgeSelection) {
-                    long elapsed = stopwatch.elapsed();
+	//
+	// game method accessors
+	//
 
-                    // add elapsed time to move time of player
-                    if (playersTurn == Game.PLAYER1) {
-                        time1 += elapsed;
-                    } else {
-                        time2 += elapsed;
-                    }
+	/**
+	 * Returns the move number.
+	 * @return the move number.
+	 */
+	public byte getMoveNr() {
+		return game.getMoveNr();
+	}
 
-                    // add move to the list of performed moves
-                    savedMoves.add(new Byte(edge));
-                    
-                    // stop game, if this was the lastmove
-                    if (game.isGameOver()) {
-                       stopGame();
-                    }
-                }
+	/**
+	 * Performs the given move for a user.
+	 * @param u
+	 *            the user.
+	 * @param m
+	 *            the move to perform.
+	 * @return the move's result.
+	 */
+	public Result makeMove(User u, Move m) {
+		return selectEdge(u, m.getEdge()) ? Result.POSITIVE : Result.NEGATIVE;
+	}
 
-                return edgeSelection;
-            }
-        }
-        return false;
-    }
+	/**
+	 * Performs the given move.
+	 * 
+	 * @param m
+	 *            the move to perform.
+	 * @return the move's result.
+	 */
+	public Result makeMove(Move m) {
+		return makeMove(null, m);
+	}
 
-    /**
-     * Returns whether it is the given user's turn to play.
-     * @param user the user.
-     * @return <code>true</code> if the game is running and it is the given
-     *          user's turn. <code>false</code> if the game is not running or
-     *          has already ended, or the given user is not allowed to make the
-     *          next move.
-     */
-    public boolean isPlayerOnTurn(User user) {
-       return isOnTurn(user, game.getPlayersTurn());
-    }
-    
-    boolean isOnTurn(User u, byte playersTurn) {
-        if (playersTurn == Game.NEUTRAL) {
-            return false; // do nothing
-        } 
-        
-        if (u != null) {
-            String nick = u.getNick();
-            if (playersTurn == Game.PLAYER1) {
-                if (!nick.equals(player1.getNick())) {
-                    return false;
-                }
-            } else if (playersTurn == Game.PLAYER2) {
-                if (!nick.equals(player2.getNick())) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
+	/**
+	 * Selects an edge in the game on the server.
+	 * 
+	 * @param u
+	 *            the user who performs the move.
+	 * @param edge
+	 *            the edge to select (between 0 and 14).
+	 * @return whether the selection has succeeded.
+	 */
+	public boolean selectEdge(User u, byte edge) {
+		if (isRunning()) {
+			byte playersTurn = game.getPlayersTurn();
+			if (isOnTurn(u, playersTurn)) {
+
+				// make move
+				boolean edgeSelection = game.selectEdge(edge);
+				if (edgeSelection) {
+					long elapsed = stopwatch.elapsed();
+
+					// add elapsed time to move time of player
+					if (playersTurn == Game.PLAYER1) {
+						time1 += elapsed;
+					} else {
+						time2 += elapsed;
+					}
+
+					// add move to the list of performed moves
+					savedMoves.add(new Byte(edge));
+
+					// stop game, if this was the lastmove
+					if (game.isGameOver()) {
+						stopGame();
+					}
+				}
+
+				return edgeSelection;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns whether it is the given user's turn to play.
+	 * @param user
+	 *            the user.
+	 * @return <code>true</code> if the game is running and it is the given
+	 *         user's turn. <code>false</code> if the game is not running or
+	 *         has already ended, or the given user is not allowed to make the
+	 *         next move.
+	 */
+	public boolean isPlayerOnTurn(User user) {
+		return isOnTurn(user, game.getPlayersTurn());
+	}
+
+	boolean isOnTurn(User u, byte playersTurn) {
+		if (playersTurn == Game.NEUTRAL) {
+			return false; // do nothing
+		}
+
+		if (u != null) {
+			String nick = u.getNick();
+			if (playersTurn == Game.PLAYER1) {
+				if (!nick.equals(player1.getNick())) {
+					return false;
+				}
+			} else if (playersTurn == Game.PLAYER2) {
+				if (!nick.equals(player2.getNick())) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
