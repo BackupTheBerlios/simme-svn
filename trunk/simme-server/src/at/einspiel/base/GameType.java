e04a634af9b033610e828b8e879208266e9f5dff
kariem <kariem@777473d5-cfd4-0310-bb17-d2de4f2d01eb>
2004-11-03 19:51:55 +0000

    added Long id

--- trunk/simme-server/src/at/einspiel/base/GameType.java
+++ trunk/simme-server/src/at/einspiel/base/GameType.java
@@ -8,49 +8,57 @@ package at.einspiel.base;
 
 /**
  * Different game types for different games. This class only holds some
- * information on the game itself (title, description). 
+ * information on the game itself (title, description).
  * 
  * @author kariem
  */
 public class GameType {
 
-   private String title;
-   private String description;
-   
+	private Long id;
+	private String title;
+	private String description;
 
-   /**
-    * Creates a new instance of <code>GameType</code>.
-    * @param title the title.
-    * @param description the description.
-    */
-   public GameType(String title, String description) {
-      super();
-      this.title = title;
-      this.description = description;
-   }
 
-   /**
-    * @return Returns the description.
-    */
-   public String getDescription() {
-      return description;
-   }
-   /**
-    * @param description The description to set.
-    */
-   public void setDescription(String description) {
-      this.description = description;
-   }
-   /**
-    * @return Returns the title.
-    */
-   public String getTitle() {
-      return title;
-   }
-   /**
-    * @param title The title to set.
-    */
-   public void setTitle(String title) {
-      this.title = title;
-   }
+	/**
+	 * @param id The id to set.
+	 */
+	private void setId(Long id) {
+		this.id = id;
+	}
+	/**
+	 * @return Returns the id.
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @return Returns the description.
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description
+	 *            The description to set.
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return Returns the title.
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @param title
+	 *            The title to set.
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
 }
